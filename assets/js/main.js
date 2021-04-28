2db3b10b9ee507588f1794b0b8a8cba24c933dbc
AndreaDex <desiderio.andrea@outlook.it>
2021-04-28 17:06:07 +0200

    completato

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,13 @@
-var lastNameList = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
+
 //1. chiedi all’utente il cognome
-var userLastName = prompt("Per favore inserisci il tuo cognome");
+var userLastName = prompt("Per favore inserisci il tuo cognome").toUpperCase();
 //2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+var lastNameList = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
 
 lastNameList.push(userLastName);
 
 //3. stampa la lista ordinata alfabeticamente
 console.log(lastNameList.sort());
-
 
 //4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
 var outputText = "";
@@ -17,6 +17,6 @@ for (var i = 0; i < lastNameList.length; i++){
     var sortedList = lastNameList.sort();
     var uppered = sortedList[i].toUpperCase();
     console.log(uppered);
-    outputText += "Sei in posizione n " + (i +1) + " " + sortedList[i] + "<br>";
+    outputText += "Sei in posizione n " + (i +1) + " " + uppered + "<br>";
 }
 document.writeln(outputText)
