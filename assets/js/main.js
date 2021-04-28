b5dc8640d3b317da19ae4f88604ee4d2165521e7
AndreaDex <desiderio.andrea@outlook.it>
2021-04-28 16:34:32 +0200

    trovata una soluzione parziale

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+var lastNameList = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
+//1. chiedi all’utente il cognome
+var userLastName = prompt("Per favore inserisci il tuo cognome");
+//2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+
+lastNameList.push(userLastName);
+
+//3. stampa la lista ordinata alfabeticamente
+console.log(lastNameList.sort());
+
+
+//4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+var outputText = "";
+
+for (var i = 0; i < lastNameList.length; i++){
+    
+    var sortedList = lastNameList.sort();
+    var uppered = sortedList[i].toUpperCase();
+    console.log(uppered);
+    outputText += "Sei in posizione n " + (i +1) + " " + sortedList[i] + "<br>";
+}
+document.writeln(outputText)
